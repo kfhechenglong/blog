54535c607ba21263417de1950d0637204ffe9f1c
hechenglong <kfhechenglong@126.com>
2021-06-29 16:14:36 +0800

    优化:wrench:

---
+++ docs/.vuepress/config-router/other.js
@@ -0,0 +1,27 @@
+// 其它文件
+module.exports = [
+  {
+    title: '其它',
+    sidebarDepth: 1,
+    collapsable: false,
+    path: '/other/vue-keep-live',
+    children: [
+      {
+        title: '1.vue实现动态缓存以及缓存销毁',
+        path: '/other/vue-keep-live'
+      },
+      {
+        title: '2.vue路由权限的控制',
+        path: '/other/vue-router-authority'
+      },
+      {
+        title: '3.文件下载',
+        path: '/other/exportFiles'
+      },
+      {
+        title: '4.数字添加千分位',
+        path: '/other/千分位'
+      }
+    ]
+  }
+]
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,4 @@
+const other = require('./config-router/other')
 module.exports = {
   title: '绝对零度随笔',
   description: 'JavaScript html css Vue',
@@ -168,30 +169,7 @@ module.exports = {
           },
         ]
       },
-      {
-        title: '其它',
-        sidebarDepth: 1,
-        collapsable: false,
-        path: '/other/vue-keep-live',
-        children: [
-          {
-            title: '1.vue实现动态缓存以及缓存销毁',
-            path: '/other/vue-keep-live'
-          },
-          {
-            title: '2.vue路由权限的控制',
-            path: '/other/vue-router-authority'
-          },
-          {
-            title: '3.文件下载',
-            path: '/other/exportFiles'
-          },
-          {
-            title: '4.数字添加千分位',
-            path: '/other/千分位'
-          }
-        ]
-      }
+      ...other
     ]
   }
 }
