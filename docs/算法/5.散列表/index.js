c11d33a1a23bee18ca0bca690f869e9743be6575
hechenglong <kfhechenglong@126.com>
2021-07-22 17:01:29 +0800

    散列

--- docs/算法/5.散列表/index.js
+++ docs/算法/5.散列表/index.js
@@ -129,9 +129,123 @@ class HashTableSeparateChaining extends HashTable {
     }
     return undefined;
   }
+
+  remove(key) {
+    const position = this.hashCode(key);
+    const linkedList = this.table[position];
+    if (linkedList != null && !linkedList.isEmpty()) {
+      let current = linkedList.getHead();
+      while(current != null) {
+        if(current.element.key === key) {
+          linkedList.remove(current.element)
+          if(linkedList.isEmpty()) {
+            delete this.table[position];
+          }
+          return true;
+        }
+        current = current.next;
+      }
+    }
+    return false;
+  }
 }
 
 const hashTable = new HashTableSeparateChaining();
 
 hashTable.put('he', 'laohe')
 console.log(hashTable.get('he'), 'HashTableSeparateChaining')
+
+
+// 线性探查
+
+class HashTableLinearProbing extends HashTable {
+  constructor(toStrFn = defaultToString) {
+    super()
+    this.toStrFn = toStrFn;
+    this.table = {};
+  }
+
+  put(key, value) {
+    if (key != null && value != null) {
+      const position = this.hashCode(key);
+      if (this.table[position] == null) {
+        this.table[position] = new valuePair(key, value);
+      } else {
+        let index = position + 1;
+        while(this.table[index] != null) {
+          index++;
+        }
+        this.table[index] = new valuePair(key, value);
+      }
+      return true;
+    }
+    return false;
+  }
+
+  get(key) {
+    const position = this.hashCode(key);
+    if(this.table[position] != null){
+      if(this.table[position].key === key) {
+        return this.table[position].value;
+      }
+      let index = position + 1;
+      while(this.table[index] != null && this.table[index].key !== key) {
+        index ++;
+      }
+      if (this.table[index] != null && this.table[index].key === key) {
+        return this.table[position].value;
+      }
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    const position = this.hashCode(key);
+    if (this.table[position] != null) {
+      if(this.table[position].key === key) {
+        delete this.table[position];
+        this.verifyRemoveSideEffect(key, position);
+        return true;
+      }
+      let index = position + 1;
+      while(this.table[index] != null && this.table[index].key !== key) {
+        index ++;
+      }
+      if (this.table[index] != null && this.table[index].key === key) {
+        delete this.table[index];
+        this.verifyRemoveSideEffect(key, index);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  verifyRemoveSideEffect(key, removedPosition) {
+    const hash = this.hashCode(key);
+    let index = removedPosition + 1;
+    while(this.table[index] != null) {
+      const posHash = this.hashCode(this.table[index].key);
+      if(posHash <= hash || posHash <= removedPosition) {
+        this.table[removedPosition] = this.table[index];
+        delete this.table[index];
+        removedPosition = index;
+      }
+      index++;
+    }
+  }
+}
+
+
+const hashTableLinearProbing = new HashTableLinearProbing();
+
+hashTableLinearProbing.put('Ygrtte'); // 4
+hashTableLinearProbing.put('Jonathan'); // 5
+hashTableLinearProbing.put('Jamie'); // 5
+hashTableLinearProbing.put('Jack'); // 7
+hashTableLinearProbing.put('Jasmine'); // 8
+hashTableLinearProbing.put('Jake'); // 9
+hashTableLinearProbing.put('Nathan'); // 10
+hashTableLinearProbing.put('Athelstan'); // 7
+hashTableLinearProbing.put('Sue'); // 5
+hashTableLinearProbing.put('Aethelwulf'); // 5
+hashTableLinearProbing.put('Sargeras'); // 10
