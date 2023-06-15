7c23c1b258e46e23b57632d9d0fbac2cdb582f2a
hechenglong <kfhechenglong@126.com>
2023-06-15 11:17:35 +0800

    fix: 🐛 修复global指向问题

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,12 @@
 import {Pagination, Card, Tag} from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-window.global = window;
-export default ({Vue, options, router }) => {
-    Vue.use(Pagination);
-    Vue.use(Card);
-    Vue.use(Tag);
+export default ({Vue, options, router, isServer }) => {
+    if(!isServer){
+        window.global = window;
+        Vue.use(Pagination);
+        Vue.use(Card);
+        Vue.use(Tag);
+    }
     router.beforeEach((to, from, next) => {
         // @pdai: 对每个页面点击添加百度统计
         if(typeof _hmt!='undefined'){
